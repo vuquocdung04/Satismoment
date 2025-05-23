4d019e13243562a08fc44896238822212e656839
Dung <vuquocdung204@gmail.com>
2025-05-23 22:13:36 +0700

    level 36

---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_4/Level_36/Level_36Ctrl.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_36Ctrl : MonoBehaviour
+{
+    public Transform popIt;
+    public int winProgress = 0;
+    public bool isWin = false;
+    Vector3 posMouse;
+    private void Update()
+    {
+        if (isWin) return;
+
+        posMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        posMouse.z = 0;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit2D hit = Physics2D.Raycast(posMouse, Vector2.zero);
+            if (hit.collider == null) return;
+            hit.collider.gameObject.SetActive(false);
+            winProgress++;
+            PlayPopAnimation();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            CheckWin();
+        }
+    }
+
+    void PlayPopAnimation()
+    {
+        popIt.DOScale(1.1f, 0.15f).SetEase(Ease.OutCubic).OnComplete(() =>
+        {
+            // Bật ra lại
+            popIt.DOScale(1f, 0.15f).SetEase(Ease.OutSine);
+        });
+    }
+
+    void CheckWin()
+    {
+        if(winProgress > 22)
+        {
+            isWin = true;
+            WinBox.SetUp().Show();
+        }
+    }
+}
