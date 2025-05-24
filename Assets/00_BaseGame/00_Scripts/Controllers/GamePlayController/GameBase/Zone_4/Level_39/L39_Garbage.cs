204427c5bd07ef9b522bfb0f55b625c2eaa00dd4
Dung <vuquocdung204@gmail.com>
2025-05-24 16:50:10 +0700

    level 39

---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_4/Level_39/L39_Garbage.cs
@@ -0,0 +1,30 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class L39_Garbage : MonoBehaviour
+{
+    public Level_39Ctrl levelCtrl;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        levelCtrl.isWin = true;
+        Debug.LogError(collision.name);
+        var paper = collision.transform;
+        paper.DOKill();
+        // Tạo hiệu ứng lăn về điểm (0,0)
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(paper.DOMove(new Vector3(0,-0.5f,0), 0.8f).SetEase(Ease.InOutSine));
+
+        seq.Join(paper.DORotate(new Vector3(0, 0, 270), 0.8f, RotateMode.FastBeyond360));
+
+
+        seq.OnComplete(() =>
+        {
+            WinBox.SetUp().Show();
+        });
+    }
+
+}
---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_4/Level_39/Level_39Ctrl.cs
@@ -0,0 +1,47 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_39Ctrl : MonoBehaviour
+{
+    public bool isWin;
+    public Transform paper;
+    public float throwDuration = 0.5f;
+    public Vector3 startPos;
+    Vector3 mousePos;
+    Vector3 curMousePos;
+
+    Vector3 directionFly;
+    void Update()
+    {
+        if (isWin) return;
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            curMousePos = mousePos;
+            directionFly = curMousePos + new Vector3(0,2,0);
+            Debug.LogError(directionFly);
+
+            Vector3 arcTarget = startPos + directionFly;
+
+            paper.DOJump(arcTarget, jumpPower: 1f, numJumps: 1, duration: throwDuration)
+                 .SetEase(Ease.OutQuad);
+
+            paper.DOScale(0.4f, throwDuration);
+
+            StartCoroutine(Create());
+
+        }
+    }
+
+    IEnumerator Create()
+    {
+        yield return new WaitForSeconds(throwDuration);
+        Transform paperPre = Instantiate(paper, startPos, Quaternion.identity);
+        paperPre.localScale = Vector3.one;
+        paper = paperPre;
+    }
+}
