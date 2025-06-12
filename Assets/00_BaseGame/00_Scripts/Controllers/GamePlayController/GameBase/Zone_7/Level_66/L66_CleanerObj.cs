2569a23480067151dac1915b54ac8603ccf8ebe1
Dung <vuquocdung204@gmail.com>
2025-06-12 10:00:17 +0700

    level 66

---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_7/Level_66/L66_CleanerObj.cs
@@ -0,0 +1,48 @@
+﻿using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L66_CleanerObj : MonoBehaviour
+{
+    public Level_66Ctrl levelCtrl;
+    public List<Transform> lsMasks;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.SetParent(transform);
+        levelCtrl.winProgress++;
+
+        if(levelCtrl.winProgress == 13)
+        {
+            levelCtrl.isWin = true;
+            DoAnimWinCodition();
+        }
+    }
+
+
+
+    public void DoAnimWinCodition()
+    {
+        Sequence animSequence = DOTween.Sequence();
+
+        animSequence.Append(this.transform.DOMove(new Vector2(-1.12f, -2.05f), 0.5f).SetEase(Ease.InOutQuad));
+        animSequence.Join(this.transform.DORotate(new Vector3(0, 0, -90f), 0.5f).SetEase(Ease.InOutQuad));
+
+        animSequence.Append(this.transform.DOMoveY(4.15f, 1f).SetEase(Ease.InOutQuad));
+        animSequence.Join(lsMasks[0].DOMoveY(6.66f, 1f).SetEase(Ease.InOutQuad));
+
+        animSequence.Append(this.transform.DORotate(new Vector3(0, 0, 90f), 0.5f).SetEase(Ease.InOutQuad));
+        animSequence.Join(this.transform.DOMove(new Vector2(1f, 2.05f), 0.5f).SetEase(Ease.InOutQuad));
+
+        animSequence.Append(this.transform.DOMoveY(-3.81f, 1f).SetEase(Ease.InOutQuad));
+        animSequence.Join(lsMasks[1].DOMoveY(-6.72f, 1f).SetEase(Ease.InOutQuad));
+
+        animSequence.AppendInterval(0.3f);
+        animSequence.OnComplete(() => HandleWinCodition());
+    }
+
+    void HandleWinCodition()
+    {
+
+        WinBox.SetUp().Show();
+    }
+}
---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_7/Level_66/Level_66Ctrl.cs
@@ -0,0 +1,46 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_66Ctrl : BaseDragController<L66_CleanerObj>
+{
+
+    public int winProgress;
+
+    [Header("Cài đặt làm mịn")]
+    [Tooltip("Tốc độ đối tượng di chuyển bám theo chuột. Càng lớn càng nhanh.")]
+    [SerializeField] private float moveSmoothSpeed = 8f;
+
+    [Tooltip("Tốc độ đối tượng xoay theo chuột. Càng lớn càng nhanh.")]
+    [SerializeField] private float rotationSmoothSpeed = 12f;
+
+    protected override void OnDragStarted()
+    {
+
+    }
+
+    protected override void OnDragLogic(Vector3 currentMousePosition, Vector3 deltaMousePosition)
+    {
+        Vector3 pivotToMouse = currentMousePosition - draggableComponent.transform.position;
+        float angleZ = Mathf.Atan2(pivotToMouse.y, pivotToMouse.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(0, 0, angleZ + 180f);
+
+        draggableComponent.transform.rotation = Quaternion.Slerp(
+            draggableComponent.transform.rotation,
+            targetRotation,
+            Time.deltaTime * rotationSmoothSpeed
+        );
+        draggableComponent.transform.position = Vector3.Lerp(
+                draggableComponent.transform.position,
+                currentMousePosition, // <- ĐÂY LÀ THAY ĐỔI QUAN TRỌNG
+                Time.deltaTime * moveSmoothSpeed
+            );
+    }
+
+    protected override void OnDragEnded()
+    {
+
+    }
+
+}
