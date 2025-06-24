1c3869d3a31d4052ddd3646f093025a9dba85844
Dung <vuquocdung204@gmail.com>
2025-06-24 22:42:28 +0700

    cp

---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_9/Level_88/Level_88Ctrl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_88Ctrl : BaseDragController<L88_GrassTractor>
+{
+    public int winProgress = 0;
+    [Header("Cài đặt làm mịn")]
+    [Tooltip("Tốc độ đối tượng di chuyển bám theo chuột. Càng lớn càng nhanh.")]
+    [SerializeField] private float moveSmoothSpeed = 8f;
+
+    [Tooltip("Tốc độ đối tượng xoay theo chuột. Càng lớn càng nhanh.")]
+    [SerializeField] private float rotationSmoothSpeed = 12f;
+    protected override void OnDragEnded()
+    {
+
+    }
+    Vector3 pivotToMouse;
+    float angleZ;
+    Quaternion targetRotation;
+    protected override void OnDragLogic(Vector3 currentMousePosition, Vector3 deltaMousePosition)
+    {
+        pivotToMouse = currentMousePosition - draggableComponent.transform.position;
+        angleZ = Mathf.Atan2(pivotToMouse.y, pivotToMouse.x) * Mathf.Rad2Deg;
+        targetRotation = Quaternion.Euler(0, 0, angleZ -90);
+
+        draggableComponent.transform.rotation = Quaternion.Slerp(
+            draggableComponent.transform.rotation,
+            targetRotation,
+            Time.deltaTime * rotationSmoothSpeed
+        );
+        draggableComponent.transform.position = Vector3.Lerp(
+                draggableComponent.transform.position,
+                currentMousePosition, // <- ĐÂY LÀ THAY ĐỔI QUAN TRỌNG
+                Time.deltaTime * moveSmoothSpeed
+            );
+    }
+
+    protected override void OnDragStarted()
+    {
+
+    }
+}
