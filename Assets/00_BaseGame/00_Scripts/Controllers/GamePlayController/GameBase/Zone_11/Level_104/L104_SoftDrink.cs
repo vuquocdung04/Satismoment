d30ade88a1fe02effb185deb8e0a69251392fb1f
Dung <vuquocdung204@gmail.com>
2025-07-01 17:57:38 +0700

    Level 104

---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_11/Level_104/L104_SoftDrink.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L104_SoftDrink : MonoBehaviour
+{
+    public BoxCollider2D boxCollider2D;
+    public Transform icon;
+    public float maxDistanceX;
+    public float posIconCorrect;
+
+    public bool CheckDistanceCorrect()
+    {
+        float distanceX = icon.localPosition.x - posIconCorrect;
+
+        // Làm tròn đến bội số gần nhất của maxDistanceX
+        float multiple = Mathf.Round(distanceX / maxDistanceX);
+        float snappedValue = multiple * maxDistanceX;
+
+        // Kiểm tra nếu gần một bội số hợp lệ
+        if (Mathf.Abs(distanceX - snappedValue) <= 0.1f)
+        {
+            // Chỉnh vị trí về điểm gần nhất
+            icon.localPosition = new Vector2(posIconCorrect + snappedValue, icon.localPosition.y);
+
+            boxCollider2D.enabled = false;
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void Init()
+    {
+        //icon = transform.Find("icon");
+        //maxDistanceX = icon.transform.GetComponent<SpriteRenderer>().bounds.size.x;
+        //boxCollider2D = GetComponent<BoxCollider2D>();
+        posIconCorrect = icon.localPosition.x;
+    }
+}
---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_11/Level_104/Level_104Ctrl.cs
@@ -0,0 +1,82 @@
+﻿using DG.Tweening;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_104Ctrl : BaseDragController<L104_SoftDrink>
+{
+    public int winProgress;
+    public List<L104_SoftDrink> lsSoftDrinks;
+    protected override void OnDragEnded()
+    {
+        if (draggableComponent.CheckDistanceCorrect())
+        {
+            winProgress++;
+            if (winProgress == lsSoftDrinks.Count)
+                StartCoroutine(HandleWinCondition());
+        }
+    }
+
+    float distance;
+    Vector3 newPos;
+    protected override void OnDragLogic(Vector3 currentMousePosition, Vector3 deltaMousePosition)
+    {
+        newPos = draggableComponent.icon.transform.localPosition + new Vector3(mouseDelta.x, 0);
+        draggableComponent.icon.transform.localPosition = newPos;
+        distance = draggableComponent.icon.transform.localPosition.x;
+        if(Mathf.Abs(distance) >= draggableComponent.maxDistanceX)
+        {
+            draggableComponent.icon.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    protected override void OnDragStarted()
+    {
+
+    }
+
+    IEnumerator HandleWinCondition()
+    {
+        isWin = true;
+        yield return StartCoroutine(PlayAnimationLoop());
+        yield return new WaitForSeconds(0.5f);
+        WinBox.SetUp().Show();
+    }
+
+    IEnumerator PlayAnimationLoop()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            foreach (var softDrink in lsSoftDrinks)
+            {
+                // Gọi hàm nhún icon
+                TweenJumpY(softDrink.icon);
+
+                yield return new WaitForSeconds(0.05f); // Đợi 0.2s trước khi icon tiếp theo
+            }
+        }
+
+    }
+    void TweenJumpY(Transform target)
+    {
+        float jumpPower = 0.2f;
+        float duration = 0.05f;
+
+        target.DOLocalMoveY(target.localPosition.y + jumpPower, duration)
+            .SetEase(Ease.Linear)
+            .OnComplete(() =>
+            {
+                target.DOLocalMoveY(target.localPosition.y - jumpPower, duration)
+                    .SetEase(Ease.Linear);
+            });
+    }
+    [Button("Setup", ButtonSizes.Large)]
+    void Setup()
+    {
+        foreach (var child in this.lsSoftDrinks)
+        {
+            child.Init();
+        }
+    }
+}
