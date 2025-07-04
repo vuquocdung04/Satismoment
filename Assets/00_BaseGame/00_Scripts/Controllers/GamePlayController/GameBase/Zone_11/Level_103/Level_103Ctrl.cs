ac50f7a9896c0f4139f046a583f323f9ad96d7ad
Dung <vuquocdung204@gmail.com>
2025-07-04 20:09:57 +0700

    fix code level 103 rotate obj

--- Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_11/Level_103/Level_103Ctrl.cs
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_11/Level_103/Level_103Ctrl.cs
@@ -14,13 +14,24 @@ public class Level_103Ctrl : BaseDragControllerVer2<L103_Ring>
                 StartCoroutine(HandleWinCondition());
         }
     }
-    float rotationAmount;
+    float angle;
+    Vector3 objectCenter;
+    Vector2 vectorToPrevMouse;
+    Vector2 vectorToCurrentMouse;
     protected override void OnDragLogic(Vector3 currentMousePosition, Vector3 deltaMousePosition)
     {
         if (!draggableComponent.isDone)
         {
-            rotationAmount = mouseDelta.y * 30f;
-            draggableComponent.transform.Rotate(0, 0, rotationAmount);
+            objectCenter = draggableComponent.transform.position;
+
+            vectorToPrevMouse = (Vector2)prevMouseWorldPos - (Vector2)objectCenter;
+
+            vectorToCurrentMouse = (Vector2)currentMousePosition - (Vector2)objectCenter;
+
+            angle = Vector2.SignedAngle(vectorToPrevMouse, vectorToCurrentMouse);
+
+
+            draggableComponent.transform.Rotate(0, 0, angle);
         }
         
     }
