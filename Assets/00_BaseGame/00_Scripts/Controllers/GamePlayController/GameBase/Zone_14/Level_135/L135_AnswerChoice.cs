b9de7b51368d3f255d65fc5148ccd1538af7ee9b
Dung <vuquocdung204@gmail.com>
2025-07-15 16:05:26 +0700

    Level 135

---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_14/Level_135/L135_AnswerChoice.cs
@@ -0,0 +1,32 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L135_AnswerChoice : MonoBehaviour
+{
+    public int id;
+    public Level_135Ctrl levelCtrl;
+    public BoxCollider2D objCollider;
+
+    public bool CheckToCorrectPosition()
+    {
+        foreach (var point in levelCtrl.lsPoints)
+        {
+            float distance = Vector2.Distance(transform.position, point.transform.position);
+            if (Mathf.Abs(distance) < 0.5f)
+            {
+                transform.DOMove(point.transform.position, 0.2f).SetEase(Ease.Linear);
+                if(point.id == id)
+                {
+                    objCollider.enabled = false;
+                    levelCtrl.correctedAnswerCount++;
+                }
+                return true;
+            }
+        }
+
+        Debug.Log("Chưa đúng vị trí nào");
+        return false;
+    }
+}
---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_14/Level_135/Level_135Ctrl.cs
@@ -0,0 +1,65 @@
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_135Ctrl : BaseDragController<L135_AnswerChoice>
+{
+    public int correctedAnswerCount;
+    public Transform hand;
+    public Transform mask;
+    public List<L135_Point> lsPoints;
+    public List<L135_AnswerChoice> lsAnswerChoices;
+    protected override void OnDragEnded()
+    {
+        if (draggableComponent.CheckToCorrectPosition())
+        {
+            if(correctedAnswerCount == lsAnswerChoices.Count)
+            {
+                isWin = true;
+                AnimationHand();
+            }
+        }
+    }
+
+    protected override void OnDragLogic(Vector3 currentMousePosition, Vector3 deltaMousePosition)
+    {
+        draggableComponent.transform.position += mouseDelta;
+    }
+
+    protected override void OnDragStarted()
+    {
+
+    }
+
+    void AnimationHand()
+    {
+        var targetMove = new Vector2(1.64f,2.91f);
+        hand.DOMove(targetMove, 1f).SetEase(Ease.Linear).OnComplete(delegate
+        {
+            mask.DOMoveX(5f,1f).SetEase(Ease.Linear);
+            hand.DOMoveX(5f,1f).SetEase(Ease.Linear);
+            StartCoroutine(HandleWinCondition());
+        });
+    }
+    IEnumerator HandleWinCondition()
+    {
+        yield return new WaitForSeconds(1f);
+        WinBox.SetUp().Show();
+    }
+
+
+    [Button("Setup",ButtonSizes.Large)]
+    void Setup()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            lsAnswerChoices[i].objCollider = lsAnswerChoices[i].transform.GetComponent<BoxCollider2D>();
+            lsAnswerChoices[i].id = i;
+            lsPoints[i].id = i;
+            lsPoints[i].transform.position = lsAnswerChoices[i].transform.position;
+            lsAnswerChoices[i].levelCtrl = this;
+        }
+    }
+}
