f41ed6312c75e2cc482d3488b8975a9c0b0195c5
Dung <vuquocdung204@gmail.com>
2025-07-23 23:35:06 +0700

    cp

---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_16/Level_157/L157_Cork.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L157_Cork : MonoBehaviour
+{
+    public void MovingY(float speed)
+    {
+
+    }
+}
