01205418577801e2373ae20fdcc663ff1b86c0aa
Dung <vuquocdung204@gmail.com>
2025-07-24 23:43:35 +0700

    cp

---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_16/Level_159/Level_159Ctrl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_159Ctrl : BaseDragController<L159_Circle>
+{
+    protected override void OnDragEnded()
+    {
+        
+    }
+
+    protected override void OnDragLogic(Vector3 currentMousePosition, Vector3 deltaMousePosition)
+    {
+        
+    }
+
+    protected override void OnDragStarted()
+    {
+        
+    }
+}
