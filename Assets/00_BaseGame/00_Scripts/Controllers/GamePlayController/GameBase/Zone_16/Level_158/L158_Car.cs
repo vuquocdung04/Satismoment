b110b20351ea906c09de67d03145f92c8f7fa82c
Dung <vuquocdung204@gmail.com>
2025-07-24 23:22:43 +0700

    Level 158: Completed

---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_16/Level_158/L158_Car.cs
@@ -0,0 +1,39 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+
+public class L158_Car : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+
+    float forwardInput;
+    Vector2 movement;
+    public void Move(Vector2 mouseDelta)
+    {
+        // Tính projection của mouse delta lên hướng forward của vật
+        forwardInput = Vector2.Dot(mouseDelta, transform.up);
+
+        movement = transform.up * forwardInput * moveSpeed;
+        rb.velocity += movement;
+    }
+
+    public void StopMovement()
+    {
+        rb.velocity = Vector2.zero;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    public void ResetCollisionState()
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    [Button("Setup", ButtonSizes.Large)]
+    void Setup()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+}
---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_16/Level_158/Level_158Ctrl.cs
@@ -0,0 +1,40 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_158Ctrl : BaseDragController<L158_Car>
+{
+    protected override void OnDragEnded()
+    {
+        draggableComponent.StopMovement();
+    }
+
+    protected override void OnDragLogic(Vector3 currentMousePosition, Vector3 deltaMousePosition)
+    {
+        draggableComponent.Move(mouseDelta);
+        CheckWin();
+    }
+
+    protected override void OnDragStarted()
+    {
+        draggableComponent.ResetCollisionState();
+    }
+
+
+    void CheckWin()
+    {
+        if(draggableComponent.transform.position.y > 4.35f)
+        {
+            isWin = true;
+            draggableComponent.transform.DOMoveY(6f,0.3f).SetEase(Ease.Linear);
+            StartCoroutine(HandleWinCondition());
+        }
+    }
+
+    IEnumerator HandleWinCondition()
+    {
+        yield return new WaitForSeconds(1f);
+        WinBox.SetUp().Show();
+    }
+}
