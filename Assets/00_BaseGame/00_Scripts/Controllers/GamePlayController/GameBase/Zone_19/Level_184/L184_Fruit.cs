f63eff695e0735bd0eb4a2d8cfe31288a31987f1
Dung <vuquocdung204@gmail.com>
2025-08-12 15:48:09 +0700

    Level 184: Completed

---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_19/Level_184/L184_Fruit.cs
@@ -0,0 +1,35 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace _00_BaseGame._00_Scripts.Controllers.GamePlayController.GameBase.Zone_19.Level_184
+{
+    public class L184_Fruit : BaseDraggableObject
+    {
+        public void CheckCollisionToJar(Transform jarTrans, float boundsSizeJar, System.Action callback = null)
+        {
+            var distance = Vector3.Distance(jarTrans.transform.position, transform.position);
+            var boundsSize = spriteRenderer.sprite.bounds.size;
+            if (distance < boundsSizeJar - boundsSize.x/2)
+            {
+                transform.DOMoveY(-3.2f,0.3f).SetEase(Ease.Linear).OnComplete(delegate
+                {
+                    objectCollider.enabled = false;
+                    callback?.Invoke();
+                });
+            }
+            else
+            {
+                OnEndDrag();
+            }
+        }
+
+        public override void ReturnToOriginalPosition()
+        {
+            objectCollider.enabled = false;
+            transform.DOMove(posDefault,0.2f).SetEase(Ease.Linear).OnComplete(delegate
+            {
+                objectCollider.enabled = true;
+            });
+        }
+    }
+}
---
+++ Assets/00_BaseGame/00_Scripts/Controllers/GamePlayController/GameBase/Zone_19/Level_184/Level_184Ctrl.cs
@@ -0,0 +1,60 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _00_BaseGame._00_Scripts.Controllers.GamePlayController.GameBase.Zone_19.Level_184
+{
+    public class Level_184Ctrl : BaseDragController<L184_Fruit>
+    {
+        public Transform jar;
+        private float boundSizeJar;
+        [SerializeField] private int currentFruitCount;
+        public List<L184_Fruit> fruits;
+        private void Start()
+        {
+            boundSizeJar = jar.GetComponent<SpriteRenderer>().sprite.bounds.size.x/2;
+        }
+
+        protected override void OnDragStarted()
+        {
+            
+        }
+
+        protected override void OnDragLogic(Vector3 currentMousePosition, Vector3 deltaMousePosition)
+        {
+            draggableComponent.transform.position += mouseDelta;
+        }
+
+        protected override void OnDragEnded()
+        {
+            draggableComponent.CheckCollisionToJar(jar,boundSizeJar, delegate
+            {
+                currentFruitCount++;
+                if (currentFruitCount == fruits.Count)
+                {
+                    isWin = true;
+                    StartCoroutine(HandleWinCondition());
+                }
+            });
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        IEnumerator HandleWinCondition()
+        {
+            yield return new WaitForSeconds(1f);
+            WinBox.SetUp().Show();
+        }
+
+        [Button("Setup Fruit", ButtonSizes.Medium)]
+        private void SetupFruit()
+        {
+            foreach (var fruit in fruits)
+            {
+                fruit.InitAfter();
+                fruit.InitBefore();
+            }
+        }
+    }
+}
